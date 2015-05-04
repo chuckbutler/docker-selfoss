0be77199efc2324bc07cbfeec37aa9a04fe33204
Charles Butler <chuck@dasroot.net>
2015-05-04 02:33:26 -0400

    Wipe /start from the start script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 9000:80 -v /home/ubuntu/dockerfile/docker-selfoss/data:/var/www/html/data selfoss /start
+docker run -d -p 9000:80 -v /home/ubuntu/dockerfile/docker-selfoss/data:/var/www/html/data selfoss
